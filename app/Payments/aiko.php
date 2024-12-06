79e0ea73ec7755a4a7c3a2763b9b9a1d3fb01f66
Aiko <aikocute@icloud.com>
2024-12-06 16:41:22 +0700

    Update new

--- app/Payments/aiko.php
+++ app/Payments/aiko.php
@@ -1,12 +1,12 @@
 <?php //002cd
 if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
 ?>
-HR+cPsLF1VPvg/tvTFUGrInCR64XUTacue8KBvB8pzNMtqhvHmJDZR1KgLotUTdwLg9DSvSkUrK2
-0T927+6TWFrMvWcn64fp5tctDO56X6JTnsHLqT6ew+s8Mn7BYewH5Is2I8/1A+56iUTrWZIQVyY8
-BkiMoS4olAKb/A7+H08oB/5AbwVHDpWTYYH+MDJpNgBalzMVtZq6/VtiCdNr0h8+EDfawcghEAFs
-g5PERVY76PGG/bbXcmhpMZUdsjr/M4hLfAYuwCKq2KpA961hjnWYyTkH3C6w9AVObeLaxD/SJQC0
-LL8ZTcrQyL9JnyG4GRPM5IqdAU36R2TA4kEJGN8SgQJ3yihbPtnijWP2trC2cqijYtavwvMMRbSb
-fgB8YoHKPT5t3X4bc13sKs7QC6i3UV/U/EzrVK4f67nSN6BB5OcPQHR/MqwWfEUG3JhEDegOrYwf
-pAxOQxQqu7YGIl+9oPtU6QOqjjZ+z2u1SiZy0/UoGk7PfVPiz0kSr83/XNfxtS3uajDCvL+tvDL7
-LDJ/SyZF73gCCNRsVYKbwhGK5sWKo1D+WWaE/lsmpPdDnRb5OcjK1XblQiTPTsgoSkm0s+67J6ff
-41fjx+OkwCDAB36TVUOlhBgKTLfh
+HR+cPxBLm6ys8Jb0lsrLJ35oJc0aM1vBa+9laut83Hj3j2UQOIWBW2rcJw6yCszDDZi0Ez0iA9wu
+bpOclCHJ3yTOG7jQWPfYiPGxKgl/TQOzclfn7zqobSouNZEvHJVAOso+7M2zjmaLHFPlVPDUsF0l
+YQOkY4TJf6pzYKh4K94CfjrHDZ+yHdhTyqJOpgdAsOr/yIZRPSehUcagnxiQYsG/1EEwlWY5ehaM
+blIDAElzzB1PpXJmS3wrKjiHtvr+V1AW31ByE7RAzt6OjWYWMfEHvK4iz3kiEwToX19EesZfIBlE
+2cl3RFFkcjNDXCAgHTNJKquY6ADVQLjeCV/GmmN6BMPO0fKJCwV4AvZIMUWhf1kjsvPz/i2gN1O9
+hFJ5Qpia6Lrhkw6/9qHdex9mZlZG8qO0KbbRfocOXlQFG6+VoGEZ6EZhGOyEzYMJ/vhmBlp3vFU9
+A2zWUKNxAIcgruzVWTVfuNJO8MGBN42QgMF/uuM5id6+4AXInSOjN/WSJybieFqIHI2a2XrQEJ4R
+6yP6lt3QxSvswJfbcBTdFGomBstJeVUMfDHsbxBO/Tc1jme8eGj3vp0zp/MXvWXsQA7BBSXmbR/3
+qiSljHNuit1FqbrvseEHuETJyIYxXtQzj0==
